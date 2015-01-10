a5b4b1ffd118f84a86538dd6d94ea742bcf722f2
David Molineus <david.molineus@netzmacht.de>
2015-01-10 13:46:40 +0100

    Fix path to initialize.php

--- assets/maps/data.php
+++ assets/maps/data.php
@@ -3,7 +3,7 @@
 use Netzmacht\Contao\Leaflet\Controller\DataController;
 
 define('TL_MODE', 'FE');
-require(dirname(dirname(dirname($_SERVER['SCRIPT_FILENAME']))) . '/system/initialize.php');
+require(dirname(dirname(dirname(dirname($_SERVER['SCRIPT_FILENAME'])))) . '/system/initialize.php');
 
 $container  = $GLOBALS['container'];
 $controller = new DataController($container['leaflet.map.service'], $container['input']);
