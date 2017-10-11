df541fe433d0d05d0bec3b2e674c103d9859827d
David Molineus <david.molineus@netzmacht.de>
2017-10-11 15:00:48 +0200

    Fix copyright year.

--- src/Mapper/OptionsBuilder.php
+++ src/Mapper/OptionsBuilder.php
@@ -5,7 +5,7 @@
  *
  * @package    contao-leaflet-maps
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2016-2017 netzmacht David Molineus. All rights reserved.
+ * @copyright  2014-2017 netzmacht David Molineus. All rights reserved.
  * @license    LGPL-3.0 https://github.com/netzmacht/contao-leaflet-maps/blob/master/LICENSE
  * @filesource
  */
