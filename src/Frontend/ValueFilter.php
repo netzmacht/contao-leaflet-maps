e27ac21902783bb397f9e70282d461f35f088e59
David Molineus <david.molineus@netzmacht.de>
2018-10-29 16:24:07 +0100

    Also replace non cachable inserttags.

--- src/Frontend/ValueFilter.php
+++ src/Frontend/ValueFilter.php
@@ -73,6 +73,9 @@ class ValueFilter
      */
     public function filter($value)
     {
-        return $this->getReplacer()->replace($value);
+        $value = $this->getReplacer()->replace($value);
+        $value = $this->getReplacer()->replace($value, false);
+
+        return $value;
     }
 }
