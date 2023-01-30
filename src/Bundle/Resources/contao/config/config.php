0c1f6a42f3e2c4a129930ec738a0013cac97019f
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2023-01-30 09:08:18 +0000

    fix undefined global variable $LEAFLET_LIBRARIES

--- src/Bundle/Resources/contao/config/config.php
+++ src/Bundle/Resources/contao/config/config.php
@@ -60,3 +60,5 @@ $GLOBALS['TL_MODELS']['tl_leaflet_marker']  = \Netzmacht\Contao\Leaflet\Model\Ma
 $GLOBALS['TL_MODELS']['tl_leaflet_popup']   = \Netzmacht\Contao\Leaflet\Model\PopupModel::class;
 $GLOBALS['TL_MODELS']['tl_leaflet_style']   = \Netzmacht\Contao\Leaflet\Model\StyleModel::class;
 $GLOBALS['TL_MODELS']['tl_leaflet_vector']  = \Netzmacht\Contao\Leaflet\Model\VectorModel::class;
+
+$GLOBALS['LEAFLET_LIBRARIES'] = [];
